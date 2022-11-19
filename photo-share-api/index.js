c8573de7b5c81239543b5600e95f3fb8b4ba15f6
Kitsuya Azuma <azuma@KitsuyanoMacBook-Pro.local>
2022-11-20 02:31:54 +0900

    [add] taggedUsers

--- photo-share-api/index.js
+++ photo-share-api/index.js
@@ -20,6 +20,7 @@ const typeDefs = `
         description: String
         category: PhotoCategory!
         postedBy: User!
+        taggedUsers: [User!]!
     }
 
     # 入力型（categoryフィールドを指定しない場合はデフォルトでPORTRAIT）
@@ -45,6 +46,7 @@ const typeDefs = `
         name: String
         avatar: String
         postedPhotos: [Photo!]!
+        inPhotos: [Photo!]!
     }
 `
 
@@ -83,6 +85,13 @@ let photos = [
     },
 ]
 
+let tags = [
+    {"photoID": "1", "userID": "gPlake"},
+    {"photoID": "2", "userID": "sSchmidt"},
+    {"photoID": "2", "userID": "mHattrup"},
+    {"photoID": "2", "userID": "gPlake"},
+]
+
 const resolvers = {
     Query: {
 
@@ -113,12 +122,27 @@ const resolvers = {
         url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
         postedBy: parent => {
             return users.find(u => u.githubLogin === parent.githubUser)
-        }
+        },
+
+        taggedUsers: parent => tags
+            // 対象の写真が関係しているタグの配列を返す
+            .filter(tag => tag.photoID === parent.id)
+            // タグの配列をユーザーIDの配列に変換する
+            .map(tag => tag.userID)
+            // ユーザーIDの配列をユーザーオブジェクトの配列に変換する
+            .map(userID => users.find(u => u.githubLogin === userID))
     },
     User: {
         postedPhotos: parent => {
             return photos.filter(p => p.githubUser === parent.githubLogin)
-        }
+        },
+        inPhotos: parent => tags
+            // 対象のユーザーが関係しているタグの配列を返す
+            .filter(tag => tag.userID === parent.id)
+            // タグの配列を写真IDの配列に変換する
+            .map(tag => tag.photoID)
+            // 写真IDの配列を写真オブジェクトの配列に変換する
+            .map(photoID => photos.find(p => p.id === photoID))
     }
 }
 
