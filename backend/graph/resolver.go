4eabf64c1cdba88cbb2074021474112dd5a8227a
Kitsuya0828 <kitsuyaazuma@gmail.com>
2023-03-23 17:35:52 +0900

    feature: sample resolver

--- backend/graph/resolver.go
+++ backend/graph/resolver.go
@@ -1,7 +1,11 @@
 package graph
 
+import "github.com/Kitsuya0828/Next.js-gqlgen-app/backend/graph/model"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct{
+	todos []*model.Todo
+}
